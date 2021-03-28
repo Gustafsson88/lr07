e9d559907b1ca2e9b07024803e1b10c0e9d69df5
gustaffson <firer.a45@gmail.com>
2021-03-29 00:20:56 +0300

    l1

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,10 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Danil Postvaykin <postvaykin01@mail.ru>
 
 #include <gtest/gtest.h>
+#include "header.hpp"
 
 TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
+    HTTP_Server first;
+    first.start();
 }
